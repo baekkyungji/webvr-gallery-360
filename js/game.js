ebead259361add1b74f0f6d3da60b5321c065f32
baekkyungji <tiffarainever@gmail.com>
2018-04-27 02:45:42 +0700

    360 Mode

--- js/game.js
+++ js/game.js
@@ -8,6 +8,8 @@ var nextPage 		= document.getElementById('nextPage');
 var dback 			= document.getElementById('detail_back');
 var d360 			= document.getElementById('detail_360');
 var ddiv 			= document.getElementById('detail_div');
+var cr 				= document.getElementById('cursor');
+
 
 var cards 			= [];
 var names 			= ['monumenkapalselam','tugupahlawan','foodjunction','suroboyocarnival'];
@@ -22,10 +24,12 @@ var columnNumber 	= 5;
 var cardNumber 		= 0;
 var animating 		= false;
 var mode360 		= false;
+var state 			= '';
 
 
 goto_home.addEventListener('click', function(){
 signaling('goto_title');
+setState('gallery');
 });
 
 nextPage.addEventListener('click', function(){
@@ -34,133 +38,190 @@ nextPage.addEventListener('click', function(){
 });
 
 cam.addEventListener('animationstart', function(){
+
 });
 cam.addEventListener('animationend', function(){
+if(state == 'gallery'){
+	mode360 = false;
+	hideDetail();
+	showGallery();
+}
+if(state == 'detail'){
+	mode360 = false;
+	hideGallery();
+	showDetail();
+}
+
+if(state == '360'){
+	mode360 = true;
+	hideGallery();
+	showDetail();
+
+}
 //hideGallery();
 //animateGallery();
 });
 
 sc.addEventListener('loaded', function(){
-createGallery();
+	createGallery();
 });
 
 dback.addEventListener('click',function(){
-signaling("back_gallery");
+	var gallery_hidden = document.getElementById('card-gallery');
+
+	console.log(gallery_hidden.getAttribute('visible'));
+	setState('gallery');
+	signaling('back_gallery');
 });
 
 d360.addEventListener('click',function(){
-mode360_on();
-mode360 = true;
+	setState('360');
+	signaling('goto_360');
+	console.log(state+'on');
+	mode360_on();
 });
 
+cam.addEventListener('doubleclick',function(){
+	alert('gff');
+});
+
+cam.addEventListener('click',function(){
+		console.log('theWorld:'+theWorld.getAttribute('visible')+' 360mode'+mode360);
+		if(mode360 == true){
+			mode360 = false;
+			//alert('GG');
+			mode360_off();
+			console.log('360 off');
+			signaling('goto_detail');
+			setState('detail');
+
+		}
+	
+});
+
+
+/*
 sky.addEventListener('click',function(){
 if(mode360 == true)
 alert('off');
 mode360_off();
 mode360 = false;
 });
+*/
 
 function signaling(str)
 {
-cam.emit(str);
+	cam.emit(str);
+}
+
+function setState(str)
+{
+	state = str;
 }
 
 function set360()
 {
-sky.setAttribute('color','blue');
+	sky.setAttribute('color','blue');
 //card.setAttribute('src','#'+names[cardNumber]);
 }
 
 function setSpace()
 {
-sky.setAttribute('src','space');
+	sky.setAttribute('src','#space');
 //card.setAttribute('src','#'+names[cardNumber]);
 }
 
 function mode360_on()
 {
-hideWorld();
-hideGallery();
-set360();
+	hideWorld();
+	hideGallery();
+	set360();
 }
 
 function mode360_off()
 {
-showWorld();
-showGallery();
-setSpace();
+	showWorld();
+	showGallery();
+	setSpace();
 }
 
 function hideGallery()
 {
-var gallery_hidden = document.getElementById('card-gallery');
-gallery_hidden.setAttribute('visible','false');	
+	var gallery_hidden = document.getElementById('card-gallery');
+	gallery_hidden.setAttribute('visible','false');	
 }
 
 function showGallery()
 {
-var gallery_hidden = document.getElementById('card-gallery');
-gallery_hidden.setAttribute('visible','true');	
+	var gallery_hidden = document.getElementById('card-gallery');
+	gallery_hidden.setAttribute('visible','true');	
 }
 
 function hideDetail()
 {
-ddiv.setAttribute('visible','false');	
+	ddiv.setAttribute('visible','false');	
+}
+
+function showDetail()
+{
+	ddiv.setAttribute('visible','true');	
 }
 
 function hideWorld()
 {
-theWorld.setAttribute('visible','false');	
+	theWorld.setAttribute('visible','false');	
 }
 
 function showWorld()
 {
-theWorld.setAttribute('visible','true');	
+	theWorld.setAttribute('visible','true');	
 }
 
 function createGallery()
 {
-var gallery  = document.createElement('a-entity');
-gallery.setAttribute('id','card-gallery');
-for (var i = 0; i < rowNumber; i++) {
-for (var j = 0; j < columnNumber; j++) {
-var card  = document.createElement('a-plane');
-card.setAttribute('position',{x:(-6+(j*3)), y:posY, z:posZ});
-card.setAttribute('scale','2 1 1');
-card.setAttribute('id',cardNumber);
-card.setAttribute('title','Monumen Kapal Selam');
-card.setAttribute('class','clickable');
-card.setAttribute('name',names[cardNumber]);
-card.setAttribute('src','#'+names[cardNumber]);
-cards.push(card);
-sc.appendChild(gallery);
-gallery.appendChild(card);
-cardNumber++;
+	var gallery  = document.createElement('a-entity');
+	gallery.setAttribute('id','card-gallery');
+	for (var i = 0; i < rowNumber; i++) {
+	for (var j = 0; j < columnNumber; j++) {
+	var card  = document.createElement('a-plane');
+	card.setAttribute('position',{x:(-6+(j*3)), y:posY, z:posZ});
+	card.setAttribute('scale','2 1 1');
+	card.setAttribute('id',cardNumber);
+	card.setAttribute('title','Monumen Kapal Selam');
+	card.setAttribute('class','clickable');
+	card.setAttribute('name',names[cardNumber]);
+	card.setAttribute('src','#'+names[cardNumber]);
+	cards.push(card);
+	sc.appendChild(gallery);
+	gallery.appendChild(card);
+	cardNumber++;
 
-card.addEventListener('click', function(evt){
-var gallerygg = document.getElementById('card-gallery');
-if(gallerygg.getAttribute('visible') == true){
-	var title;
-	var description = 'value:'+desc;
-	//console.log(description);
-	signaling("goto_detail");
-	//alert(evt.detail.target.id);
-	//sky.setAttribute('color','blue');
-	title = 'value:'+titles[evt.detail.target.id].toString();
-	detail_title.setAttribute('text-geometry',title);
-	detail_desc.setAttribute('text-geometry',description);
-}
+	card.addEventListener('click', function(evt){
+	state = 'detail';
+	var gallerygg = document.getElementById('card-gallery');
+	if(gallerygg.getAttribute('visible') == true){
+		var title;
+		var description = 'value:'+desc;
+		console.log(evt);
+		signaling("goto_detail");
+		//alert(evt.detail.target.id);
+		//sky.setAttribute('color','blue');
+		title = 'value:'+titles[evt.detail.target.id];
+		detail_title.setAttribute('text-geometry',title);
+		detail_desc.setAttribute('text-geometry',description);
+	}
 });
 
 
-card.addEventListener('mouseenter', function(evt){
-//alert('sdsdsdsds');
+	card.addEventListener('mouseenter', function(evt){
+	//alert('sdsdsdsds');
 });
 }
-posY = posY - 1.5;
+	posY = posY - 1.5;
 }
-var cardAnim  = document.createElement('a-animation');
-cardAnim.setAttribute('id','cardAnim');
+
+	var cardAnim  = document.createElement('a-animation');
+	cardAnim.setAttribute('id','cardAnim');
 }
 
 
@@ -176,14 +237,13 @@ function animateGallery()
 
 function checkID()
 {
-var initCount = 0;
-
-for (var i = 0; i < rowNumber; i++) {
-for (var j = 0; j < columnNumber; j++) {
-alert(cards[initCount].id+'gg');
-initCount++;
-}
-}
+	var initCount = 0;
+	for (var i = 0; i < rowNumber; i++) {
+	for (var j = 0; j < columnNumber; j++) {
+	alert(cards[initCount].id+'gg');
+	initCount++;
+		}
+	}
 
 }
 
@@ -194,3 +254,4 @@ box.addEventListener('click', function(){
 box.parentNode.removeChild(box);
 });
 */
+
